0cce7664f16084101c791db10916c08ea2086c8f
Dhananjaya Bulathsinghala <dhananjaya@fixico.com>
2025-10-19 14:07:31 +0530

    Migration files created

---
+++ database/migrations/2025_10_19_075030_create_customers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('external_id', 64)->unique();
+            $table->string('email')->index();
+            $table->string('name');
+
+            // Timestamps with microsecond precision (helps job ordering & debugging)
+            $table->timestamps(6);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
